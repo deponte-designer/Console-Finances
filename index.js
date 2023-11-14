f954dfbbeecf7a24fe02e947423a50f8e964cca3
Ponte, Nadia <deponte.work@gmail.com>
2023-11-14 22:18:52 +0000

    Copy and paste the tasks into comments

--- index.js
+++ index.js
@@ -86,3 +86,35 @@ var finances = [
   ['Jan-2017', 138230],
   ['Feb-2017', 671099],
 ];
+
+
+
+// * Create a new GitHub repo called Console-Finances. Then, clone it to your computer. 
+
+// * Copy the starter files in your local git repository.
+
+// * You have been given a dataset composed of arrays with two fields, Date and Profit/Losses.
+// Your task is to write JavaScript code that analyzes the records to calculate each of the following:
+
+// ! The total number of months included in the dataset.
+
+// ! The net total amount of Profit/Losses over the entire period.
+
+// ! The average of the changes in Profit/Losses over the entire period.
+
+// ! You will need to track what the total change in Profit/Losses are from month to month and then find the average.
+// (Total/(Number of months - 1))
+
+// ! The greatest increase in Profit/Losses (date and amount) over the entire period.
+
+// ! The greatest decrease in Profit/Losses (date and amount) over the entire period.
+
+// ! When you open your code in the browser your resulting analysis should look similar to the following:
+
+// Financial Analysis 
+// ----------------
+// Total Months: 86
+// Total: $38382578
+// Average Change: -2315.12
+// Greatest Increase in Profits/Losses: Feb-2012 ($1926159)
+// Greatest Decrease in Profits/Losses: Sep-2013 ($-2196167)
